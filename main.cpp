d9e6c4f38034b71c73766ba5e5fa832bd2233a68
Miguel Sória da Luz <MiguelSoria348@gmail.com>
2026-01-20 12:41:45 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <Arduino.h>
 #include <driver/adc.h>
 #include <iostream>
+
 #define RELE 12
 #define amp_out 32
 
 void setup() {
     pinMode(RELE, OUTPUT);
-    adc1_config_width(ADC_WIDTH_BIT_12);
+    analogReadResolution(12);
+    analogSetAttenuation(ADC_11db);
     Serial.begin(9600);
 
 }
@@ -16,30 +18,27 @@ void loop() {
   digitalWrite(RELE, HIGH); //high relé fechado, low relé aberto
   //digitalWrite(RELE, LOW); //high relé fechado, low relé aberto 
 
-  int raw_amp_out[512], raw_amp_out_sum=0;
-  float v_amp, Isample[512], I=0;
-  const float Gain=4.28, analogReadRange=4096, Vcc=3.3; // ganho do ina, ADC de 12 bits, Vcc do esp32
+  uint32_t raw_amp_out_sum=0;
+  float I=0;
 
   for(uint16_t i=0; i<512; i++) { // média de 512 amostras
-    raw_amp_out[i] = analogRead(amp_out);  // [sem escala]
-    v_amp = (Vcc * raw_amp_out[i]) / analogReadRange; // [V]
-    Isample[i] = (1000 * v_amp / Gain) / 0.1;        // I [mA], 0.1 = resistencia do shunt
-
-    I=I + Isample[i];
-    raw_amp_out_sum += raw_amp_out[i];
+      raw_amp_out_sum += analogRead(amp_out); 
+      delayMicroseconds(100);
   }
-  I = I / 512; // valor médio
+  float raw_average = (float)raw_amp_out_sum / 512.0; // valor médio
+  I = 0.0015355*raw_average + 0.24712;
+
+  if (raw_average < 50) I = 0;
 
   Serial.print("Corrente = ");
-  Serial.print(I);
-  Serial.println(" mA");
+  Serial.print(I, 3);
+  Serial.println(" A");
 
   Serial.print("Valor bruto do ADC = ");
-  Serial.println(raw_amp_out_sum/512);
+  Serial.println(raw_average);
 
   Serial.println("-----------------------");
 
-  I=0;
   delay(1000);
   
 }
